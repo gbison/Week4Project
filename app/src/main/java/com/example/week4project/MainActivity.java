bd45ab4b0a1f8764c738cf5b914b11a571e50438
Bryan Ison <bryan@sgmediasoft.com>
2019-09-14 20:17:13 -0400

    * Initial Update of the APP

---
+++ app/src/main/java/com/example/week4project/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.week4project;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.sip.SipSession;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button nextButton;
+    TextView questionView;
+    RadioGroup buttonGroup;
+    Question currentQuestion;
+    AlertDialog.Builder builder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Get our STATIC objects we have placed in the Activity scene
+        nextButton      = findViewById(R.id.NextButton);
+        questionView    = findViewById(R.id.QuestionView);
+        buttonGroup     = findViewById(R.id.ButtonGroup);
+    }
+
+    public void OnNextClick(View v)
+    {
+        // clear the current content
+        currentQuestion = null;
+        questionView.setText("");
+        buttonGroup.clearCheck();
+        buttonGroup.removeAllViews();
+
+        // create new content
+        currentQuestion= new Question(getApplicationContext());
+        currentQuestion.AddGroup(buttonGroup);
+        questionView.setText(currentQuestion.getQuestion());
+    }
+
+    public void CheckAnswer(View v)
+    {
+        System.out.println(buttonGroup.getCheckedRadioButtonId());
+        System.out.println(currentQuestion.getCorrectAnswer());
+        if(buttonGroup.getCheckedRadioButtonId() == currentQuestion.getCorrectAnswer())
+        {
+            CharSequence text = "CORRECT";
+            //Question was answered correctly!
+            Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG);
+            toast.show();
+
+            ShowDialog("Congratulations!", "You answered CORRECTLY!");
+        }
+        else
+        {
+            //They got it wrong!87455*-\
+            Toast toast = Toast.makeText(getApplicationContext(), "WRONG", Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
+
+    public void ShowDialog(String title, String message)
+    {
+        builder = new AlertDialog.Builder(this);
+        builder.setTitle(title);
+        builder.setMessage(message);
+
+        // add a button
+        builder.setPositiveButton("OK", null);
+
+        // create and show the alert dialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+}
---
+++ app/src/main/java/com/example/week4project/Question.java
@@ -0,0 +1,121 @@
+package com.example.week4project;
+
+import android.content.Context;
+import android.provider.MediaStore;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+public class Question {
+
+    //Declarations
+    private String a;
+    private String b;
+    private String c;
+    private String d;
+
+    private String question;
+
+    private RadioButton buttonA;
+    private RadioButton buttonB;
+    private RadioButton buttonC;
+    private RadioButton buttonD;
+
+    private int correctAnswer;
+
+    public int getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(int correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    //Getters & Setters
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    public String getC() {
+        return c;
+    }
+
+    public void setC(String c) {
+        this.c = c;
+    }
+
+    public String getD() {
+        return d;
+    }
+
+    public void setD(String d) {
+        this.d = d;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    //Constructor
+    public Question(Context context){
+
+        //Here we are hardcoding this values, ideally you want to format a text file that you
+        //could read values from so that you could plug question sets into the app.
+        a = "Sir George Boole";
+        b = "Sir Anthony Hopkins";
+        c = "King Henry VIII";
+        d = "Leonardo Divinci";
+
+        question = "Who was the inventor of Boolean logic?";
+
+        //Configure constant IDS, if we dont the group box will keep incrementing them over time.
+        final int AID = 10;
+        final int BID = 20;
+        final int CID = 30;
+        final int DID = 40;
+
+        //Create buttons and associate answers
+        buttonA = new RadioButton(context);
+        buttonA.setText(a);
+        buttonA.setId(AID);
+        buttonB = new RadioButton(context);
+        buttonB.setText(b);
+        buttonB.setId(BID);
+        buttonC = new RadioButton(context);
+        buttonC.setText(c);
+        buttonC.setId(CID);
+        buttonD = new RadioButton(context);
+        buttonD.setText(d);
+        buttonD.setId(DID);
+
+        //define the correct answer
+        correctAnswer = AID; //first object in the RadioGroup
+    }
+
+    //General Methods.
+    public void AddGroup(RadioGroup buttonGroup)
+    {
+        //Add to group
+        buttonGroup.addView(buttonA);
+        buttonGroup.addView(buttonB);
+        buttonGroup.addView(buttonC);
+        buttonGroup.addView(buttonD);
+    }
+
+
+}
